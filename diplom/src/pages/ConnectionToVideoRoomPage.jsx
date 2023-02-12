2ef956ae431fc919f3b9360705831c63ddc09fc0
DanilSergeev <danil.sergeew5555@yandex.ru>
2023-02-12 19:49:02 +0300

    -

--- diplom/src/pages/ConnectionToVideoRoomPage.jsx
+++ diplom/src/pages/ConnectionToVideoRoomPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import socket from "../socket"
 import ACTIONS from "../socket/actions.js"
 import { v4 } from "uuid"
@@ -7,26 +7,30 @@ import { useNavigate } from 'react-router-dom';
 const ConnectionToVideoRoomPage = () => {
     const navigate = useNavigate()
     const [rooms, setRooms] = useState([])
+    const rootNode = useRef()
+
     useEffect(() => {
         socket.on(ACTIONS.SHARE_ROOMS, ({ rooms = [] } = {}) => {
-            setRooms(rooms)
+            if (rootNode.current) {
+                setRooms(rooms)
+            }
         })
     }, [])
 
     return (
-        <main>
+        <main ref={rootNode}>
             <h2>Видео комнаты</h2>
             <ul>
                 {
                     rooms.map(item =>
                         <li key={item}>
                             {item}
-                            <button onClick={()=>{navigate("/videoRoom/"+item)}}>JOin</button>
+                            <button onClick={() => { navigate("/videoRoom/" + item) }}>JOin</button>
                         </li>
                     )
                 }
             </ul>
-            <button onClick={()=>{navigate("/videoRoom/"+v4())}}>Новая комната</button>
+            <button onClick={() => { navigate("/videoRoom/" + v4()) }}>Новая комната</button>
         </main>
     )
 }
--- diplom/src/pages/VideoRoomPage.jsx
+++ diplom/src/pages/VideoRoomPage.jsx
@@ -1,22 +1,47 @@
 import { useParams } from "react-router"
 import useWebRTC, { LOCAL_VIDEO } from "../hooks/useWebRTC"
 
+function layout(clientNumber = 1) {
+    const pairs = Array.from({ length: clientNumber }).reduce((acc, next, index, arr) => {
+        if (index % 2 === 0) {
+             acc.push(arr.slice(index, index + 2))
+        }
+        return acc
+    }, [])
+
+    const rowsNumber = pairs.length
+    const height = `${100 / rowsNumber}%`
+
+    return pairs.map((row, index, arr) => {
+        if (index === arr.length - 1 && row.length === 1) {
+            return [{ width: "100%", height }]
+        }
+        return row.map(() => ({
+            width: "50%", height
+        }))
+    }).flat()
+}
+
+
 const VideoRoomPage = () => {
-    const {id: roomID} = useParams()
-    const {clients , provideMediaRef} = useWebRTC(roomID)
+    const { id: roomID } = useParams()
+    const { clients, provideMediaRef } = useWebRTC(roomID)
+    const videoLayout = layout(clients.length)
+
+
     return (
-        <main>
-            {clients.map(clientID=>{
-                return <div>
-                    <video 
-                    width='100%'
-                    height='100%'
-                    ref={instance => {
-                      provideMediaRef(clientID, instance);
-                    }}
-                    autoPlay
-                    playsInline
-                    muted={clientID === LOCAL_VIDEO}
+        <main className="videoRoomMain">
+            {clients.map((clientID, index) => {
+                return <div key={clientID} style={videoLayout[index]}>
+                    <video
+                        width='100%'
+                        height='100%'
+                        ref={instance => {
+                            provideMediaRef(clientID, instance);
+                        }}
+                        autoPlay
+                        playsInline
+                        muted={clientID === LOCAL_VIDEO}
                     ></video>
                 </div>
             })}
